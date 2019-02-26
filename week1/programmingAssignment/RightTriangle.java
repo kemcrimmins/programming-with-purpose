8b80aac8f15b4924738a62c8af0a17018c3cebb5
Kem Crimmins <kem.crimmins@gmail.com>
2019-02-26 04:25:03 -0500

    add ordering of input RightTriangle.java

--- week1/programmingAssignment/RightTriangle.java
+++ week1/programmingAssignment/RightTriangle.java
@@ -6,10 +6,27 @@ public class RightTriangle
     int b = Integer.parseInt(args[1]);
     int c = Integer.parseInt(args[2]); 
 
-    if ((a <= 0) || (b <= 0) || (c <= 0)) 
+    if ((a <= 0) || (b <= 0) || (c <= 0)) // Check for negative integers
       System.out.println(false); 
-    else 
-      System.out.println((a*a+b*b==c*c));
+    else
+     { 
+       // Make sure that c is the largest integer
+       int temp;
+       if (a > b) 
+       {
+         temp = a;
+         a = b;
+         b = temp;
+       }
+       if (b > c) 
+       {
+         temp = b;
+         b = c;
+         c = temp;
+       }
+       // test for right triangle 
+       System.out.println((a*a+b*b==c*c)); 
+     }
   }
 }
  
