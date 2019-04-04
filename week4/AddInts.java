5b1351b320b417555419298c82137283f858f03d
Kem Crimmins <kem.crimmins@gmail.com>
2019-04-04 06:40:44 -0400

    use Sedgewick stdout library

--- week4/AddInts.java
+++ week4/AddInts.java
@@ -6,7 +6,7 @@ public class AddInts
     int sum = 0;
     for (int i = 0; i < n; i++)
     {
-      int value = StdIn.readln();
+      int value = StdIn.readInt();
       sum += value;
     }
     StdOut.println("Sum is " + sum);
